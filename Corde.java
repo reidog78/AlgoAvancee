ea7f1311596f5036446f32c135aa6b3f7591244a
Tibo <thibaut.gaudier78@gmail.com>
2020-03-11 18:07:37 +0100

    Corde

---
+++ Corde.java
@@ -0,0 +1,31 @@
+public class Corde {
+
+    private int s1;
+    private int s2;
+
+    public Corde(int s1, int s2) {
+        if (s1 != s2) {
+            this.s1 = s1;
+            this.s2 = s2;
+        }
+    }
+
+    public int getS1() {
+        return s1;
+    }
+
+    public void setS1(int s1) {
+        this.s1 = s1;
+    }
+
+    public int getS2() {
+        return s2;
+    }
+
+    public void setS2(int s2) {
+        this.s2 = s2;
+    }
+
+    
+
+}
