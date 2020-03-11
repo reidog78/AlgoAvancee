7e051073a3fcdaf5253772bb3f484b8bf32a5934
Simon LERIN <slerin@enssat.fr>
2020-03-11 17:37:14 +0100

    Implémentation de valideCorde

--- Polygone.java
+++ Polygone.java
@@ -24,7 +24,27 @@ public class Polygone {
         this.p = p;
     }
 
-    
+    public Boolean valideCorde(int s1, int s2, int s3, int s4) {
+        Boolean returnValue = true;
+        if (s2<s1){
+            int temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+        if (s4<s3){
+            int temp = s3;
+            s3 = s4;
+            s4 = temp;
+        }
+
+        if(s1==s3 && s2==s4 || s1==s4 && s2==s3){
+            returnValue = false;
+        }
+        else if(s1<s3 && s3<s2 && s2<s4){
+            returnValue = false;
+        }
+        return returnValue;
+    }
 
 
 }
