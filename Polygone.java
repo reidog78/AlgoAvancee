913c735c01141d48b82655fecc88a8317a4dde6f
Tibo <thibaut.gaudier78@gmail.com>
2020-03-11 17:38:29 +0100

    triangulerSuccessifs

--- Polygone.java
+++ Polygone.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Polygone {
 
     private int nbSommets;
@@ -16,14 +19,22 @@ public class Polygone {
         this.nbSommets = nbSommets;
     }
 
-    public Point[] getP() {
-        return p;
-    }
+    public void triangulerSuccessifs () {
 
-    public void setP(Point[] p) {
-        this.p = p;
-    }
+        ArrayList<Integer[]> triangulation = new ArrayList<>();
 
+        for (int i = 0; i < nbSommets; i++) {
+            for (int j = 0; j < nbSommets; i++) {
+                for (int k = 0; k < triangulation.size(); k++)
+                if (valideCorde(i, j, triangulation.get(k)[0], triangulation.get(k)[1])) {
+                    Integer[] l;
+                    l[0] = i;
+                    l[1] = j;
+                    triangulation.add(l);
+                }
+            }
+        }
+    }
     
 
 
