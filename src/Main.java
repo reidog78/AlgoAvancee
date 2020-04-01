66dab6c3ca5acd631acc8bfb37bd8823d8f4d567
Tibo <thibaut.gaudier78@gmail.com>
2020-04-01 10:55:05 +0200

    gitignore + structure

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
         pts.add(new Point(0, 0));
         pts.add(new Point(0, 10));
         pts.add(new Point(10, 10));
+        pts.add(new Point(15, 5));
+        pts.add(new Point(12, 3));
         pts.add(new Point(10, 0));
 
 
--- src/Polygone.java
+++ src/Polygone.java
@@ -51,8 +51,8 @@ public class Polygone {
         return c;
     }
 
-    public Boolean valideCorde(int s1, int s2) {
-        Boolean returnValue = true;
+    public boolean valideCorde(int s1, int s2) {
+        boolean returnValue = true;
         
         if (s2<s1){
             int temp = s1;
@@ -66,18 +66,16 @@ public class Polygone {
         if(s1==(s2-1)%nbSommets || s1 == (s2+1)%nbSommets){
             returnValue = false;
         }
-        for (int i=0 ; i<c.size(); i++){
-            System.out.println(s1 + " " + s2 + " " + c.get(i));
-            if(s1==c.get(i).getS1() && s2==c.get(i).getS2() || s1==c.get(i).getS2() && s2==c.get(i).getS1()){
+        for (Corde corde : c) {
+            System.out.println(s1 + " " + s2 + " " + corde);
+            if (s1 == corde.getS1() && s2 == corde.getS2() || s1 == corde.getS2() && s2 == corde.getS1()) {
+                returnValue = false;
+            } else if (s1 < corde.getS1() && corde.getS1() < s2 && s2 < corde.getS2()) {
+                returnValue = false;
+            } else if (s1 > corde.getS1() && corde.getS2() > s1 && s2 > corde.getS2()) {
                 returnValue = false;
             }
-            else if(s1<c.get(i).getS1() && c.get(i).getS1()<s2 && s2<c.get(i).getS2()){
-                returnValue = false;
-            }
-            else if(s1>c.get(i).getS1() && c.get(i).getS2()>s1 && s2>c.get(i).getS2()){
-                returnValue = false;
-            }
-            
+
         }
 
         System.out.println(returnValue);
